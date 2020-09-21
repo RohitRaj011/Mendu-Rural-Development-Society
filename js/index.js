d1b13b5cdff936fd0405727a2cfc3ca24776ebe2
Rohit Raj <rohit011@protonmail.com>
2020-09-21 20:24:31 +0530

    updated

--- js/index.js
+++ js/index.js
@@ -1,4 +1,24 @@
 window.addEventListener("scroll", function () {
-    var navbar = document.getElementsByClassName("container-fluid")[0];
-    navbar.classList.toggle("sticky", window.scrollY > 0);
+  var navbar = document.getElementsByClassName("container-fluid")[0];
+  navbar.classList.toggle("sticky", window.scrollY > 0);
+});
+
+$(document).ready(function () {
+  var page_url = window.location.href;
+  var page_id = page_url.substring(page_url.lastIndexOf("#") + 1);
+  if (page_id == "programme") {
+    $("html, body").animate(
+      {
+        scrollTop: $("#scroll-" + page_id).offset().top - 200,
+      },
+      800
+    );
+  } else if (page_id == "about") {
+    $("html, body").animate(
+      {
+        scrollTop: $("#scroll-" + page_id).offset().top,
+      },
+      800
+    );
+  }
 });
